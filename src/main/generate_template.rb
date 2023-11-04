eb6a53e692141c4af0a724fabd91af5628e7b072
Martin Klapper <martinklapper@tutanota.com>
2023-11-04 11:29:12 +0100

    fix exception catching, still no handling

--- src/main/generate_template.rb
+++ src/main/generate_template.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
-require '../modules/parser/code_problem_parser_factory'
-require '../modules/file_generator/file_generator_factory'
+require_relative '../modules/platform_finder/platform_finder'
+require_relative '../modules/platform_finder/unrecognized_platform_error'
+
+require_relative '../modules/parser/code_problem_parser_factory'
+# require_relative '../modules/file_generator/file_generator_factory'
+
 
 def main
 
+  # TODO: Catching errors is there but they are not actually handled
   # TODO: Sanitize input when used in shell command-execution and handle errors
   # TODO: After catching an error at a given position, continue execution at a reasonable point
   while true
@@ -13,8 +18,8 @@ def main
       puts 'What is the url of the code-problem?'
       problem_url = gets.chomp
       problem_platform = PlatformFinder.determine_platform(problem_url)
-    rescue
-      puts "An error occured #{e.message}"
+    rescue UnrecognizedPlatformError => e
+      puts "An error occured: #{e.message}"
     end
 
     # TODO: Is language supported?
@@ -22,8 +27,8 @@ def main
     begin
       puts 'Which language would you like to train in?'
       problem_lang = gets.chomp
-    rescue
-      puts "An error occured #{e.message}"
+    rescue StandardError => e
+      puts "An error occured: #{e.message}"
     end
 
     # TODO: Does Platform-Language Combination exist for this problem?
@@ -32,16 +37,16 @@ def main
       parser = CodeProblemParserFactory.create_parser(problem_platform)
       parser.parse_page(problem_url, problem_lang)
       problem_information = parser.problem_details
-    rescue
-      puts "An error occured #{e.message}"
+    rescue StandardError => e
+      puts "An error occured: #{e.message}"
     end
 
     # generate the templates for kata and user language
     begin
       file_generator = FileGeneratorFactory.create_generator(problem_lang)
       file_generator.generate_template_files(problem_information)
-    rescue
-      puts "An error occured #{e.message}"
+    rescue StandardError => e
+      puts "An error occured: #{e.message}"
     end
   end
 end
