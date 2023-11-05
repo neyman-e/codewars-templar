be7eb72e5bd116f2ac70d0ffabda79998dfad766
Martin Klapper <martinklapper@tutanota.com>
2023-11-05 11:55:33 +0100

    Language Prompt as Selection + minor fixes

--- src/main/generate_template.rb
+++ src/main/generate_template.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 require_relative '../modules/platform_finder/platform_finder'
 require_relative '../modules/platform_finder/unrecognized_platform_error'
 
 require_relative '../modules/parser/code_problem_parser_factory'
-# require_relative '../modules/file_generator/file_generator_factory'
+require_relative '../modules/parser/html_is_empty_error'
 
+# require_relative '../modules/file_generator/file_generator_factory'
 
 def main
 
+  # Open user config to access stuff
+  user_config = YAML.load_file(File.absolute_path(File.dirname(__FILE__) + '/../../config/my_config.yml'))
+
+  # TODO: Maybe implement MVC as to not bloat this main? would prob need a router and "model" is not clear
   # TODO: Catching errors is there but they are not actually handled
     # there could also be some mini-logging implementation
   # TODO: Sanitize input when used in shell command-execution and handle errors
-  # TODO: After catching an error at a given position, continue execution at a reasonable point
-  while true
+  # TODO: After catching an error at a given position, continue execution at a reasonable point (Exception Handling)
+  loop do
 
     begin
       puts 'What is the url of the code-problem?'
@@ -23,22 +30,31 @@ def main
       puts "An error occured: #{e.message}"
     end
 
-    # TODO: Is language supported?
-    # TODO: Is platform-language combination supported?
+    # TODO: Handle Exception if there user-input is invalid
     begin
       puts 'Which language would you like to train in?'
-      problem_lang = gets.chomp
+      puts 'Enter the corresponding number'
+
+      langs_with_templates = user_config.reject { |key, value| key == 'settings' }.keys
+      langs_with_templates.each_with_index do |lang, index|
+        puts "#{index + 1} - #{lang}"
+      end
+
+      problem_lang = langs_with_templates[gets.chomp.to_i - 1]
+
+    # TODO: Does not do anything right now
     rescue StandardError => e
       puts "An error occured: #{e.message}"
     end
 
     # TODO: Does Platform-Language Combination exist for this problem?
     # scrape code-problem details from the url for given language
+    # TODO Catch and handle selenium errors (Invalid urls, non-existing language etc.)
     begin
       parser = CodeProblemParserFactory.create_parser(problem_platform)
       parser.parse_page(problem_url, problem_lang)
       problem_information = parser.problem_details
-    rescue StandardError => e
+    rescue HTMLIsEmptyError => e
       puts "An error occured: #{e.message}"
     end
 
