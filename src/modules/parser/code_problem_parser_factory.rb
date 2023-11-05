6618c50a6cfac490541e77ee3f815de13aa4ad09
Martin Klapper <martinklapper@tutanota.com>
2023-11-05 13:58:14 +0100

    style fix and rephrasing of errormessage

--- src/modules/parser/code_problem_parser_factory.rb
+++ src/modules/parser/code_problem_parser_factory.rb
@@ -3,13 +3,12 @@
 require_relative 'codewars_problem_parser'
 
 class CodeProblemParserFactory
-
   def self.create_parser(problem_platform)
     case problem_platform
     when 'codewars'
-      return CodewarsProblemParser.new
+      CodewarsProblemParser.new
     else
-      raise NotImplementedError, "#{problem_platform} is not supported (yet)"
+      raise NotImplementedError, "No Parser for this platform '#{problem_platform}' exists yet"
     end
   end
 end
